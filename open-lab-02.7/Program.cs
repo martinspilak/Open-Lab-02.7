6ef7c322e45108f0b4b4d9d430f224adfd4c3d32
martinspilak <martinecko24@gmail.com>
2019-10-19 13:39:36 +0200

    open-lab-02.7

---
+++ open-lab-02.7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace open_lab_02._7
+{
+    class Program
+    {
+
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        {
+            return (true);
+        }
+
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine());
+            if (month == 12)
+            {
+                if (day == 24)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
